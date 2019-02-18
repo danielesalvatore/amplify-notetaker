f6644c9a29e2ffc7244fbf5d01341e704409bbcb
Daniele Salvatore <danieleslvtr@gmail.com>
2019-02-18 19:27:39 +0100

    Add remove note fn

--- src/App.js
+++ src/App.js
@@ -1,51 +1,94 @@
 import React, {Component} from 'react';
 import {withAuthenticator} from 'aws-amplify-react'
+import {API, graphqlOperation} from 'aws-amplify'
+import {createNote, deleteNote} from "./graphql/mutations"
+import {listNotes} from "./graphql/queries";
 
 class App extends Component {
 
-  state = {
-    notes: [{
-      id: 1,
-      note: "Hello world!"
-    }]
-  };
+    state = {
 
-  render() {
+        note: "",
 
-    const {notes} = this.state;
+        notes: []
+    };
 
-    return (
-        <div className="flex flex-column items-center justify-center pas3 bg-washed-red">
-          <h1 className="code f2-l">Amplify Notetaker</h1>
-          <form className="mb3">
+    handleChangeNote = event => this.setState({note: event.target.value});
 
-            <input
-                type="text"
-                className="pa3 f4"
-                placeholder="Write your note!"
-            />
+    handleAddNote = async event => {
+        const {note, notes} = this.state;
+        event.preventDefault();
 
-            <button
-                type="submit"
-                className="pa2 f4">
-              Add note
-            </button>
-          </form>
+        const input = {note};
 
-          {notes.map(item => (
-              <div key={item.id} className="flex items-center">
-                <li className="list pa1 f3">
-                  {item.note}
-                </li>
+        const result = await API.graphql(graphqlOperation(createNote, {input}));
+        const newNote = result.data.createNote;
+        const updatedNote = [newNote, ...notes];
 
-                <button className="bg-transparent f4 bn">
-                  <span>&times;</span>
-                </button>
-              </div>
-          ))}
-        </div>
-    );
-  }
+        this.setState({notes: updatedNote, note: ""});
+    };
+
+    handleDeleteNote = async nodeId => {
+        const {notes} = this.state;
+        const input = {id: nodeId};
+        const result = await API.graphql(graphqlOperation(deleteNote, {input}));
+        const deletedNote = result.data.deleteNote.id;
+
+        const updatedNotes = notes.filter(note => note.id !== deletedNote);
+        this.setState({notes: updatedNotes})
+    };
+
+    async componentDidMount() {
+        const results = await API.graphql(graphqlOperation(listNotes));
+        this.setState({notes: results.data.listNotes.items})
+    }
+
+    render() {
+
+        const {notes, note} = this.state;
+
+        return (
+            <div className="flex flex-column items-center justify-center pas3 bg-washed-red">
+
+                <h1 className="code f2-l">Amplify Notetaker</h1>
+
+                <form
+                    onSubmit={this.handleAddNote}
+                    className="mb3"
+                >
+
+                    <input
+                        type="text"
+                        className="pa3 f4"
+                        placeholder="Write your note!"
+                        onChange={this.handleChangeNote}
+                        value={note}
+                    />
+
+                    <button
+                        type="submit"
+                        className="pa2 f4">
+                        Add note
+                    </button>
+                </form>
+
+                {notes.map(item => (
+                    <div key={item.id} className="flex items-center">
+                        <li className="list pa1 f3">
+                            {item.note}
+                        </li>
+
+                        <button
+                            className="bg-transparent f4 bn"
+                            onClick={() => this.handleDeleteNote(item.id)}
+                        >
+                            <span>&times;</span>
+                        </button>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default withAuthenticator(App, {includeGreetings: true});
